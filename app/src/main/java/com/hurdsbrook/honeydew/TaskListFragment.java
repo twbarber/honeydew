e43cea391c1674c884bbd3e25c22178b3221ca89
Tyler Barber <tylerwbarber@gmail.com>
2015-12-11 19:03:28 -0600

    Adding RecylcerView and Adapter to Task List

--- app/src/main/java/com/hurdsbrook/honeydew/TaskListFragment.java
+++ app/src/main/java/com/hurdsbrook/honeydew/TaskListFragment.java
@@ -2,12 +2,15 @@ package com.hurdsbrook.honeydew;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Fragment used to display list of Tasks.
@@ -29,5 +32,61 @@ public class TaskListFragment extends Fragment {
             Toast.makeText(getContext(), taskToToast.getName(), Toast.LENGTH_SHORT).show();
         }
         return v;
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
+
+        private List<Task> mTasks;
+
+        public TaskAdapter(List<Task> tasks) {
+            mTasks = tasks;
+        }
+
+        @Override
+        public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(R.layout.task_list_item, parent, false);
+            return new TaskHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(TaskHolder holder, int position) {
+            Task task = mTasks.get(position);
+            holder.bindTask(task);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mTasks.size();
+        }
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private TextView mTitleTextView;
+        private Task mTask;
+
+        public TaskHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            mTitleTextView = (TextView)
+                    itemView.findViewById(R.id.list_item_task_name_text_view);
+
+        }
+
+        public void bindTask(Task task) {
+            mTitleTextView.setText(task.getName());
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(),
+                    mTask.getName() + " clicked!", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
